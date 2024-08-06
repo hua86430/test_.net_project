aef85fa4f32acef982a80b8be1e4079d71baba33
Hua <k0981465867@gmail.com>
2024-08-06 22:58:04 +0800

    test

---
+++ test_python_run_1/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using test_python_run_1.Models;
+
+namespace test_python_run_1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    public IActionResult Index()
+    {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return View();
+        return View();
+        return View();
+        return View();
+        return View();
+        return View();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ test_python_run_1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace test_python_run_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
